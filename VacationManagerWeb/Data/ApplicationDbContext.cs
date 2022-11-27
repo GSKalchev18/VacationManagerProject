f1a74762227a0ca4160bfe15d36fdad6c198817e
Dani Kompanski <gskalchev18@codingburgas.bg>
2022-11-27 19:10:31 +0200

    Configure Connection String

---
+++ VacationManagerWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VacationManagerWeb.Models;
+
+namespace VacationManagerWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Teams> Teams { get; set; }
+        public DbSet<Vacations> Vacations { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
