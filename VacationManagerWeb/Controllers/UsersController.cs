7cc8d20714fcbec7af16ec4261bb6d4a4db5ead5
Dani Kompanski <gskalchev18@codingburgas.bg>
2022-11-27 21:29:53 +0200

    Add index page for users

--- VacationManagerWeb/Controllers/UsersController.cs
+++ VacationManagerWeb/Controllers/UsersController.cs
@@ -1,17 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
+using VacationManagerWeb.Data;
+using VacationManagerWeb.Models;
 
 namespace VacationManagerWeb.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Users> odjUsersList = _db.Users.ToList();
+            return View(odjUsersList);
         }
 
         public IActionResult Create()
         {
             return View("CreateUser");
         }
+
+        public IActionResult Edit()
+        {
+            return View("EditUser");
+        }
+
+        public IActionResult Details()
+        {
+            return View("UserDetails");
+        }
+
     }
 }
