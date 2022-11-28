70ad00687b9effee8e03ca624518fd2ca9885df3
Dani Kompanski <gskalchev18@codingburgas.bg>
2022-11-28 19:29:42 +0200

    Add create Team functionality

---
+++ VacationManagerWeb/Controllers/TeamsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using VacationManagerWeb.Data;
+using VacationManagerWeb.Models;
+
+namespace VacationManagerWeb.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TeamsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Teams> odjTeamsList = _db.Teams.ToList();
+            return View(odjTeamsList);
+        }
+
+        public IActionResult Create()
+        {
+            //ViewBag.TeamLeader = User;
+            return View("CreateTeam");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Teams obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Teams.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ VacationManagerWeb/Migrations/20221128163645_AddVirtualList.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VacationManagerWeb.Migrations
+{
+    public partial class AddVirtualList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeamsId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TeamsId",
+                table: "Users",
+                column: "TeamsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Teams_TeamsId",
+                table: "Users",
+                column: "TeamsId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Teams_TeamsId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_TeamsId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TeamsId",
+                table: "Users");
+        }
+    }
+}
