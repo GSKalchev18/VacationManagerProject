8489d5b4c182ac00f78d2559c94a5287e622660d
Dani Kompanski <gskalchev18@codingburgas.bg>
2022-11-28 22:56:32 +0200

    Add creating project

---
+++ VacationManagerWeb/Controllers/ProjectsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VacationManagerWeb.Data;
+using VacationManagerWeb.Models;
+
+namespace VacationManagerWeb.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProjectsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Projects> odjPeojectsList = _db.Projects.ToList();
+            return View(odjPeojectsList);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.TeamId = new SelectList(_db.Teams, "Id", "Name");
+            return View("CreateProject");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Projects obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Projects.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var ProjectFromDb = _db.Projects.Find(id);
+            return View("DeleteTeam", ProjectFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Projects.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Projects.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
